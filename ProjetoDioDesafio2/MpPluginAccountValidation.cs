c2f04f4acdea19c24bb9ba1588a76388b54625db
MarcusVPR <marcus_vpr@hotmail.com>
2023-11-10 11:48:16 -0300

    feat: Dio Desafio 2 CRM

---
+++ ProjetoDioDesafio2/MpPluginAccountValidation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xrm.Sdk;
+
+namespace ProjetoDioDesafio2
+{
+    public class MpPluginAccountValidation {
+        //
+
+    }
+}
